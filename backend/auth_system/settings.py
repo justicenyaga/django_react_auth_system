d4b22c1e3018f73079882799ba6d00625f234dd1
Justice Nyaga <justiceevansmwema@gmail.com>
2023-05-15 18:08:56 +0300

    add default permission classes

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -136,6 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
