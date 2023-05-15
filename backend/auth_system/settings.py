91450b48663f1eb5c36cb1b0eb2c7377a3a78c2d
Justice Nyaga <justiceevansmwema@gmail.com>
2023-05-15 12:12:27 +0300

    add accounts app and create a custom user model

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    'rest_framework',
+    'djoser',
+
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +126,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'accounts.UserAccount'
