164964f1beb2287b8cf48d27b3098625e1001afd
Justice Nyaga <justiceevansmwema@gmail.com>
2023-05-17 11:03:06 +0300

    update HomePage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
   return (
     <div className="container my-5">
       <div className="p-5 text-center bg-body-tertiary rounded-3">
@@ -10,24 +13,29 @@ const HomePage = () => {
           This is an incredible auth system with production level features!
         </p>
 
-        <p>
-          Click Sign In to login to your account or Sign Up to create a new
-          account.
-        </p>
-        <div className="d-inline-flex gap-2 mb-5">
-          <Link
-            className="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill"
-            to="/login"
-          >
-            Sign In
-          </Link>
-          <Link
-            className="btn btn-outline-secondary btn-lg px-4 rounded-pill"
-            to="/signup"
-          >
-            Sign Up
-          </Link>
-        </div>
+        {!isAuthenticated && (
+          <>
+            <p>
+              Click Sign In to login to your account or Sign Up to create a new
+              account.
+            </p>
+
+            <div className="d-inline-flex gap-2 mb-5">
+              <Link
+                className="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill"
+                to="/login"
+              >
+                Sign In
+              </Link>
+              <Link
+                className="btn btn-outline-secondary btn-lg px-4 rounded-pill"
+                to="/signup"
+              >
+                Sign Up
+              </Link>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
