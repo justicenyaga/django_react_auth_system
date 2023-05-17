ae997c2037e9bb2a6b5da143b27420914186b9b2
Justice Nyaga <justiceevansmwema@gmail.com>
2023-05-17 18:28:41 +0300

    update User model - break name into first & last name

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, **extra_fields)
 
         user.set_password(password)
         user.save()
@@ -18,20 +18,21 @@ class UserAccountManager(BaseUserManager):
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserAccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def get_full_name(self):
-        return self.name
+        return self.first_name
 
     def get_short_name(self):
-        return self.name
+        return self.first_name
 
     def __str__(self):
         return self.email
