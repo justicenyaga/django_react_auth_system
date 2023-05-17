4bca3e5e51bcd79f25c3dfd6ac9269f74021d07d
Justice Nyaga <justiceevansmwema@gmail.com>
2023-05-17 08:45:57 +0300

    improve homepage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="container my-5">
+      <div className="p-5 text-center bg-body-tertiary rounded-3">
+        <h1 className="text-body-emphasis">Welcome to Auth System!</h1>
+        <p className="col-lg-8 mx-auto fs-5 text-muted">
+          This is an incredible auth system with production level features!
+        </p>
+
+        <p>
+          Click Sign In to login to your account or Sign Up to create a new
+          account.
+        </p>
+        <div className="d-inline-flex gap-2 mb-5">
+          <Link
+            className="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill"
+            to="/login"
+          >
+            Sign In
+          </Link>
+          <Link
+            className="btn btn-outline-secondary btn-lg px-4 rounded-pill"
+            to="/signup"
+          >
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
